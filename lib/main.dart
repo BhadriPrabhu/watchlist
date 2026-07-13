114a9a50fb8700de19b6a263b404dcbc70256a65
Bhadri Prabhu K <bhadrikeshav0612@gmail.com>
2026-07-14 02:30:00 +0530

    feat: added the listview with checkbox

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+// import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MainApp());
+}
+
+class FutureList {
+  String name;
+  bool isCompleted;
+  FutureList({required this.name, this.isCompleted = false});
 }
 
 class MainApp extends StatelessWidget {
@@ -10,9 +16,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: WatchlistScreen(),
-    );
+    return const MaterialApp(home: WatchlistScreen());
   }
 }
 
@@ -24,12 +28,50 @@ class WatchlistScreen extends StatefulWidget {
 }
 
 class _WatchlistScreenState extends State<WatchlistScreen> {
+  List<FutureList> myFutureList = [
+    FutureList(name: "Spider Man"),
+    FutureList(name: "Avengers"),
+  ];
 
   // final TextEditingController _textContoller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("WatchList", style: TextStyle(color: Colors.white, fontWeight: FontWeight.w900)), toolbarHeight: 50.0, backgroundColor: Colors.blueAccent,),
+      appBar: AppBar(
+        title: Text(
+          "WatchList",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.w900),
+        ),
+        toolbarHeight: 50.0,
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: (ListView.builder(
+        itemCount: myFutureList.length,
+        itemBuilder: (context, index) {
+          final myList = myFutureList[index];
+          return InkWell(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20.0,
+                vertical: 6.0,
+              ),
+              child: Row(
+                children: [
+                  Checkbox(
+                    value: myList.isCompleted,
+                    onChanged: (bool? newValue) {
+                      setState(() {
+                        myList.isCompleted = newValue ?? false;
+                      });
+                    },
+                  ),
+                  Text(myList.name, style: TextStyle(decoration: myList.isCompleted ? TextDecoration.lineThrough : TextDecoration.none),),
+                ],
+              ),
+            ),
+          );
+        },
+      )),
     );
   }
 }
